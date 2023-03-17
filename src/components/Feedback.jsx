b03d672bdee6be94acedaacf95cc0e046901c202
Ulrich Mabou <ulrichmabou@MacBook-Pro-2.local>
2023-03-17 19:21:27 +0200

    add send button for prompt in feedback component

--- src/components/Feedback.jsx
+++ src/components/Feedback.jsx
@@ -1,10 +1,26 @@
-import React from 'react'
+import { useState } from 'react'
+import { FcNext } from "react-icons/fc";
 
 const Feedback = (props) => {
+
+    const [promptSent, setPromptSent] = useState(false);
+
+    function sendPrompt() {
+        setPromptSent(true);
+        console.log("sending...");
+    }
+
     return (
         <div className="feedback-container">
-            <p className="feedback-text">You selected: {props.selection && <span>{props.selection}</span>}</p>
-            {props.selection && <p className="feedback-prompt">
+            {props.selection && <p className="feedback-text">You selected: <span>{props.selection}</span></p>}
+            {props.selection &&
+                <div className="next">
+                    <button className="next-btn" onClick={sendPrompt}>
+                        <FcNext />
+                    </button>
+                </div>
+            }
+            {promptSent && <p className="feedback-prompt">
                 You are someone who prefers {props.selection === props.optionAText ? props.selection : props.optionBText} over {props.selection === props.optionAText ? props.optionBText : props.optionAText}.</p>}
             {props.description && <p className="feedback-description">{props.description}</p>}
         </div>
